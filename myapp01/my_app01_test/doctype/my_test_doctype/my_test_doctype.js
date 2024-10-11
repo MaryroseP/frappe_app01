41fb8ef5f19295c83a58505a671b99e6abfc7ff9
MaryroseP <105261229+MaryroseP@users.noreply.github.com>
2024-10-11 02:42:23 +0000

    This is a test doctype

---
+++ myapp01/my_app01_test/doctype/my_test_doctype/my_test_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Maryrose Pergis and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
